461a9c214c53b099cf2ddb79ba9fbcbf47e5e1a1
Szymon Kędzierski <kedzierski.szymon@yahoo.com>
2019-06-16 16:57:07 +0200

    Add patrol to 6 policemen

--- source/Game/TrainLevel.cpp
+++ source/Game/TrainLevel.cpp
@@ -42,6 +42,8 @@ namespace Rendering
 			this->getListOfNode().at(0)->AddTriggerCollider(gmObj->getCollider());
 		}
 
+		int policemanSize = 0;
+
 		// Add point Lights to soldiers
 		for (int i = 0; i < gameObjects.size(); i++)
 		{
@@ -58,6 +60,17 @@ namespace Rendering
 			{
 				Policeman* policeman = gameObjects.at(i)->As<Policeman>();
 				GetPolicemans().push_back(policeman);
+				/*
+				std::vector<XMFLOAT2> patrolPoints = std::vector<XMFLOAT2>();
+				patrolPoints.push_back(XMFLOAT2(-62, -178));
+				patrolPoints.push_back(XMFLOAT2(182, -178));
+				patrolPoints.push_back(XMFLOAT2(182, -326));
+				patrolPoints.push_back(XMFLOAT2(-62, -326));
+
+				policeman->Patrol(patrolPoints);
+				*/
+				SetPatrol(policeman, policemanSize);
+				policemanSize++;
 
 				mPointLights.push_back(policeman->GetPointLight());
 			}
@@ -75,24 +88,21 @@ namespace Rendering
 				//mSpotLights.push_back(doubleStreetLampPostWithMegaphone->GetSpotLight());
 				mPointLights.push_back(doubleStreetLampPostWithMegaphone->GetPointLight());
 			}
-		}
-
+ 		}		
 
 		for (int i = 0; i < GameObjects.size(); i++)
 		{
 			GameObject* gameObject = GameObjects.at(i)->As<GameObject>();
 			gameObject->SetLightsReferences(GetDirectionalLights(), GetPointLights(), GetSpotLights());
-			gameObject->Initialize();
+			//gameObject->Initialize();
 		}
 
 		Colliders* collD = new Colliders();
 		collD->setTriggerReaction(PAINTING_POSITION, { 45.f, -7.f, 22.f }, { 9.f, 2.f, 9.f });
-		this->getListOfNode().at(0)->AddTriggerCollider(collD);
-
-		//SetPatrol();
+		this->getListOfNode().at(0)->AddTriggerCollider(collD);		
 	}
 
-	void TrainLevel::SetPatrol()
+	void TrainLevel::SetPatrol(Policeman* policeman, int index)
 	{
 		std::vector<XMFLOAT2> patrolPoints1 = std::vector<XMFLOAT2>();
 		patrolPoints1.push_back(XMFLOAT2(-62, -178));
@@ -101,17 +111,15 @@ namespace Rendering
 		patrolPoints1.push_back(XMFLOAT2(-62, -326));
 
 		std::vector<XMFLOAT2> patrolPoints2 = std::vector<XMFLOAT2>();
-		patrolPoints1.push_back(XMFLOAT2(-62, -178));
-		patrolPoints1.push_back(XMFLOAT2(182, -178));
-		patrolPoints1.push_back(XMFLOAT2(182, -326));
-		patrolPoints1.push_back(XMFLOAT2(-62, -326));
+		patrolPoints2.push_back(XMFLOAT2(215, -220));
+		patrolPoints2.push_back(XMFLOAT2(215, 231));
+		patrolPoints2.push_back(XMFLOAT2(231, 231));
+		patrolPoints2.push_back(XMFLOAT2(231, -220));
+		
+		if (index == 2 || index == 3 || index == 4 || index == 5)
+			policeman->Patrol(patrolPoints1);
 
-		GetPolicemans().at(2)->As<Policeman>()->Patrol(patrolPoints1);
-		GetPolicemans().at(3)->As<Policeman>()->Patrol(patrolPoints1);
-		GetPolicemans().at(4)->As<Policeman>()->Patrol(patrolPoints1);
-		GetPolicemans().at(5)->As<Policeman>()->Patrol(patrolPoints1);
-
-		GetPolicemans().at(6)->As<Policeman>()->Patrol(patrolPoints1);
-		GetPolicemans().at(7)->As<Policeman>()->Patrol(patrolPoints1);
+		if (index == 6 || index == 7)
+			policeman->Patrol(patrolPoints2);
 	}
 }
